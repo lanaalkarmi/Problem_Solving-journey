2bdec2e8d892ecd716ed2ecdb2a8f3ef224d8ef2
lana alkarmi <lanakarmi05@gmail.com>
2026-06-20 17:22:23 +0300

    Add files via upload

---
+++ 20_6_2026__problem1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t; cin >> t;
+    while (t--) {
+
+        int k;
+        cin >> k;
+        int c = 0;
+        int num = 1;
+        while (c < k) {
+            if (num % 3 != 0 && num % 10 != 3) {
+                c++;
+                if (c == k) {
+                    cout << num << endl;
+                }
+            }
+            num++;
+        }
+    }
+}
